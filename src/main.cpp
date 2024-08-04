815b964a415a410c230257895e1ffce5a844c05c
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-08-03 22:03:10 -0700

    First moving object

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Test Win", sf::Style::Default, settings);
     sf::RectangleShape pipe(sf::Vector2f(50.f, 100.f));
     pipe.setFillColor(sf::Color::Green);
-
+    pipe.setPosition(1280.f, 360.f - pipe.getSize().y / 2);
+    
+    float speed = 200.f;
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -17,6 +20,18 @@ int main()
         {
             if (vnt.type == sf::Event::Closed)
                 window.close();
+        }
+        
+        sf::Time deltaTime = clock.restart();
+        
+        
+float distance = speed * deltaTime.asSeconds();
+        pipe.move(-distance, 0); // Move left
+
+        // If the rectangle goes off-screen, reposition it to the right
+        if (pipe.getPosition().x + pipe.getSize().x < 0)
+        {
+            pipe.setPosition(1280.f, 360.f - pipe.getSize().y / 2);
         }
 
         window.clear();
